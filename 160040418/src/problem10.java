3a1eb0c29772cb8e564078e0d404b9e5a4d1dd7b
Sirisha <Sirisha@DESKTOP-8L3DD56>
2019-06-25 10:59:28 +0530

    simple java programs

---
+++ 160040418/src/problem10.java
@@ -0,0 +1,8 @@
+
+public class problem10 {
+	public static void main(String[]args){
+		int i;
+		for(i=1;i<=10;i++)
+		System.out.print("\t"+i);
+		}
+		}
---
+++ 160040418/src/problem3.java
@@ -0,0 +1,12 @@
+
+public class problem3 {
+		public static void main(String[]args){
+		if(args.length==0)
+		System.out.println("No values");
+		else
+		{
+		for(String i:args)
+		System.out.print(i+",");
+		}
+		}
+}
